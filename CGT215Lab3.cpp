a15f7e7fa875737d775418d6f046e0e0887980ba
Hao Liang <liang393@purdue.edu>
2022-09-13 15:59:15 -0400

    添加项目文件。

---
+++ CGT215Lab3.cpp
@@ -0,0 +1,69 @@
+﻿// CGT215Lab3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value:";
+	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
+	// The rest of this function is an exercise to the reader
+}
+void firstChoice(float A, float B) {
+	float C = A + B;
+	cout << A << "+" << B << "=" << C << endl;
+}
+void secondChoice(float A, float B) {
+	float C = A - B;
+	cout << A << "-" << B << "=" << C << endl;
+}
+void thirdChoice(float A, float B) {
+	float C = A * B;
+	cout << A << "*" << B << "=" << C << endl;
+}
+void fourthChoice(float A, float B) {
+	float C = A / B;
+	cout << A << "/" << B << "=" << C << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		firstChoice(A, B);
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		fourthChoice(A, B);
+	}
+	return 0;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
